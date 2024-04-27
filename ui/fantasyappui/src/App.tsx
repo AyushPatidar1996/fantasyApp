18f84954bc796c530182a67ca4697927804de49d
AyushPatidar1996 <39326878+AyushPatidar1996@users.noreply.github.com>
2024-04-27 13:37:29 +0530

    Routing setup done

--- ui/fantasyappui/src/App.tsx
+++ ui/fantasyappui/src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import LoginPage from "./components/loginPage/LoginPage";
 
 const App = () => {
+  const router = createBrowserRouter([{ path: "/", element: <LoginPage /> }]);
   return (
     <div className="App">
+      <RouterProvider router={router} />
     </div>
   );
-}
+};
 
 export default App;
